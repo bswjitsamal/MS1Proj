c0f57489c5213e0177d9734c765183a5c76e299c
bswjitsamal@gmail.com <Biswa@1984>
2024-04-14 22:41:32 +0530

    4th Commit 14th Apr

--- catalog-service/src/test/java/com/sivalabs/bookstore/catalog/ContainersConfig.java
+++ catalog-service/src/test/java/com/sivalabs/bookstore/catalog/ContainersConfig.java
@@ -1,6 +1,5 @@
 package com.sivalabs.bookstore.catalog;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +7,11 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestCatalogServiceApplication {
+public class ContainersConfig {
 
-
-    public static void main(String[] args) {
-        SpringApplication.from(CatalogServiceApplication::main)
-                .with(ContainersConfig.class)
-                .run(args);
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
     }
 }
